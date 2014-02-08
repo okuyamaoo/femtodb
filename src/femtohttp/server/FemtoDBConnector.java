221fb31ced6b821633d28e5fdc39835df977ff64
okuyamaoo <ta.okuyamaoo@gmail.com>
2014-02-09 01:36:26 +0900

    femtohttpserver add

---
+++ src/femtohttp/server/FemtoDBConnector.java
@@ -0,0 +1,15 @@
+package femtohttp.server;
+
+
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FemtoDBConnector  extends HttpServlet { 
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
---
+++ src/femtohttp/server/FemtoHttpServer.java
@@ -0,0 +1,43 @@
+package femtohttp.server;
+
+
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+public class FemtoHttpServer {
+
+    private static String uri = "/femtoserver";
+    private static int port = -8080;
+    private static int maxThreads = 120;
+    
+    public static void main(String[] args) {
+        try {
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startServer(String[] args) throws Exception {
+    
+        QueuedThreadPool threadPool = new QueuedThreadPool();
+        threadPool.setMaxThreads(FemtoHttpServer.maxThreads);
+        Server server = new Server(threadPool);
+
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        //handler.addServletWithMapping(femtohttp.server.FemtoDBConnector.class, "/femtoserver");
+
+        ServerConnector http = new ServerConnector(server);
+        http.setPort(8080);
+        http.setIdleTimeout(30000);
+        server.addConnector(http);
+        server.start();
+        server.join();
+    }
+
+
+}
