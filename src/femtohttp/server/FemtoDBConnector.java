7d3f395d7f5d79022797c8097a701e6d0110b362
Takahiro Iwase <iwase@kdl.co.jp>
2014-02-09 18:28:46 +0900

    FemtoServer tmplate add

--- src/femtohttp/server/FemtoDBConnector.java
+++ src/femtohttp/server/FemtoDBConnector.java
@@ -11,5 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class FemtoDBConnector  extends HttpServlet { 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>FemtoServer</h1>");
     }
 }
