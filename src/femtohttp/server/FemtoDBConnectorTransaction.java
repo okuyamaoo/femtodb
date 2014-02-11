2f303caf30427db932fbf0e874ab363d8c1180d4
Takahiro Iwase <iwase@kdl.co.jp>
2014-02-12 00:27:15 +0900

    Http server mod

---
+++ src/femtohttp/server/FemtoDBConnectorTransaction.java
@@ -0,0 +1,111 @@
+package femtohttp.server;
+
+
+
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import femtodb.core.*;
+import femtodb.core.table.*;
+import femtodb.core.table.transaction.*;
+import femtodb.core.table.data.*;
+import femtodb.core.table.type.*;
+import femtodb.core.accessor.*;
+import femtodb.core.accessor.parameter.*;
+
+public class FemtoDBConnectorTransaction  extends HttpServlet { 
+
+    /** 
+     * トランザクションを新規で作成する.<br>
+     * 返却値としてTransaction番号が返される.<br>
+     * 本リクエストはパラメータを必要としない.<br>
+     * 返却値はJSONフォーマットで{"transactionno":1}となり左辺の数値Long値であり可変となる<br>
+     * ここで取得した数値を以降のデータアクセス系やcommit、rollback、トランザクション終了などのパラメータとして利用する<br>
+     *
+     * @param request
+     * @param response 
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        TransactionNo tn = FemtoHttpServer.dataAccessor.createTransaction();
+        long transactionNo = tn.getTransactionNo();
+        StringBuilder strBuf = new StringBuilder();
+        strBuf.append("{\"transactionno\":");
+        strBuf.append(transactionNo);
+        strBuf.append("}");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().println(strBuf.toString());
+    }
+
+    /** 
+     * トランザクションをCommitもしくはRollbackする.<br>
+     * 返却値として成否が返される.<br>
+     * 本リクエストはパラメータとして以下を必要とする.<br>
+     * "transactionno" 対象とするトランザクションの番号 <br>
+     * "method" "commit"及び、"rollback"を指定<br>
+     *<br><br>
+     * 返却値はJSONフォーマットで{"result":"true"}となり左辺の文字列は"true" or "false"である<br>
+     *<br>
+     * URLの例 /femtodb/transaction?transactionno=23&method=commit<br>
+     *
+     * @param request
+     * @param response 
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String transactionNo = request.getParameter("transactionno");
+        long transactioNoLong = -1L;
+        try {
+            transactioNoLong = new Long(transactionNo).longValue();
+        } catch (Exception e2) {
+            StringBuilder strBuf = new StringBuilder();
+            strBuf.append("{\"result\":\"");
+            strBuf.append("false");
+            strBuf.append("\"}");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/json; charset=utf-8");
+            response.getWriter().println(strBuf.toString());
+            return;
+        }
+        String method = request.getParameter("method");
+        boolean result = false;
+
+        if (method.equals("commit")) {
+            // commit処理
+            result = FemtoHttpServer.dataAccessor.commitTransaction(new Long(transactionNo).longValue());
+        } else if (method.equals("rollback")) {
+            // rollback処理
+            result = FemtoHttpServer.dataAccessor.rollbackTransaction(new Long(transactionNo).longValue());
+        }
+        
+        StringBuilder strBuf = new StringBuilder();
+        strBuf.append("{\"result\":\"");
+        strBuf.append(result);
+        strBuf.append("\"}");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().println(strBuf.toString());
+    }
+
+    /**
+     * femtoserver?
+     *
+     *
+     *
+     */
+    private void executeRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
+}
--- src/femtohttp/server/FemtoHttpServer.java
+++ src/femtohttp/server/FemtoHttpServer.java
@@ -1,17 +1,25 @@
 package femtohttp.server;
 
-
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.servlet.ServletHandler;
+
+import femtodb.core.*;
+import femtodb.core.table.*;
+import femtodb.core.table.transaction.*;
+import femtodb.core.table.data.*;
+import femtodb.core.table.type.*;
+import femtodb.core.accessor.*;
+import femtodb.core.accessor.parameter.*;
 
 public class FemtoHttpServer {
 
     private static String uri = "/femtoserver";
     private static int port = -8080;
     private static int maxThreads = 120;
+    
+    static DataAccessor dataAccessor = null;
     
     public static void main(String[] args) {
         try {
@@ -23,14 +31,18 @@ public class FemtoHttpServer {
     }
 
     private void startServer(String[] args) throws Exception {
-    
+
+        // DataAccessorを初期化
+        FemtoHttpServer.dataAccessor = new DataAccessor();
+
         QueuedThreadPool threadPool = new QueuedThreadPool();
         threadPool.setMaxThreads(FemtoHttpServer.maxThreads);
         Server server = new Server(threadPool);
 
         ServletHandler handler = new ServletHandler();
         server.setHandler(handler);
-        handler.addServletWithMapping(femtohttp.server.FemtoDBConnector.class, "/femtoserver");
+        handler.addServletWithMapping(femtohttp.server.FemtoDBConnectorTransaction.class, "/femtodb/transaction");
+        handler.addServletWithMapping(femtohttp.server.FemtoDBConnectorDataaccess.class, "/femtodb/dataaccess");
 
         ServerConnector http = new ServerConnector(server);
         http.setPort(8080);
